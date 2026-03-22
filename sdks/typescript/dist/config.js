d24b66f65f996bdb46d4fb302d250e2fe80ad046
riken127 <henriquenoronha05@gmail.com>
2026-03-22 20:41:49 +0000

    chore(repo): stop tracking generated TypeScript artifacts
    Removes checked-in sdks/typescript/dist and sdks/typescript/node_modules content from version control now that ignore rules and build scripts are in place.
    
    This keeps repository history lean and prevents dependency/build-output churn from polluting release diffs.

--- sdks/typescript/dist/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.defaultConfig = void 0;
-exports.defaultConfig = {
-    host: 'localhost',
-    port: 50051,
-    useTls: false,
-    timeoutMs: 5000
-};
